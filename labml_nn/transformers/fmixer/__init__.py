a10ae8975d525de5318cfb910c63c70a3ce94a5b
lixu13 <lixu13@baidu.com>
2021-06-15 20:35:26 +0800

    sync

--- labml_nn/transformers/fmixer/__init__.py
+++ labml_nn/transformers/fmixer/__init__.py
@@ -144,7 +144,7 @@ class MIXFFT(nn.Module):
         # Apply the Fourier transform along the sequence dimension
         # $$\mathcal{F}_\text{seq} \big(\mathcal{F}_\text{hidden} (x) \big)$$
         x = torch.fft.fft(fft_hidden, dim=0)
-        x = self.channel_mixer(x)
+        # x = self.channel_mixer(x)
         x = self.token_mixer(x)
         return x
 
