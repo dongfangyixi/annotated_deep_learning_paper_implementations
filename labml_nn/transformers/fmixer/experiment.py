c2d8ea6fe9977f10d6aaebb616468fb7ec353132
lixu13 <lixu13@baidu.com>
2021-06-15 19:02:49 +0800

    sync

--- labml_nn/transformers/fmixer/experiment.py
+++ labml_nn/transformers/fmixer/experiment.py
@@ -123,6 +123,7 @@ def main():
 
         # Train for $32$ epochs
         'epochs': 32,
+        'batch_size': 32,
         # Switch between training and validation for $10$ times
         # per epoch
         'inner_iterations': 10,
