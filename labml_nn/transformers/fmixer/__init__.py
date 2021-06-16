1b1cb8f78ab85b32b32920402fdcc61490c06a51
lixu13 <lixu13@baidu.com>
2021-06-16 11:29:14 +0800

    sync

--- labml_nn/transformers/fmixer/__init__.py
+++ labml_nn/transformers/fmixer/__init__.py
@@ -236,12 +236,12 @@ class MIXER(nn.Module):
         assert mask is None
         x = query
         # channel mixer fft
-        # x = torch.fft.fft(x, dim=2)
+        x = torch.fft.fft(x, dim=2)
         # Apply the Fourier transform along the sequence dimension
         # $$\mathcal{F}_\text{seq} \big(\mathcal{F}_\text{hidden} (x) \big)$$
         # token mixer fft
         # x = torch.fft.fft(x, dim=0)
-        x = self.channel_mixer(x)
+        # x = self.channel_mixer(x)
         x = self.pfft_token_mixer(x)
 
         return x
